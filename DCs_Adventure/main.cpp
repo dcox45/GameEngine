598c77404077e5444358fcd26a5ac699afc95228
dcox45 <dylanwjc@gmail.com>
2019-12-17 23:09:37 -0500

    tweaking dependencies on DCs_Adventure

--- DCs_Adventure/main.cpp
+++ DCs_Adventure/main.cpp
@@ -1,6 +1,5 @@
 #include <Engine.h>
-#include <ECS.h>
-#include <Components.h>
+
 
 int main()
 {
@@ -15,10 +14,11 @@ int main()
 
 	for (int j = 0; j < 10; j++) {
 		ent = gameEngine.world->create();
-		ent->assign<Transform>(i * 25, j * 32);
-		ent->assign <Sprite>("../Debug/herosheet.png");
+		ent->assign<Transform>(25, 32);
+		ent->assign<Sprite>("../Debug/herosheet.png");
 		ent->assign<Animator>(32, 32, 200.f);
 	}
 	
 	gameEngine.start(&win);
+	return 0;
 }
